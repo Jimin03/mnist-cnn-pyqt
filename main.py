19d18475525053eb85c43d1dddec182bbc13290e
Jimin03 <jiminyoun03@gmail.com>
2022-12-09 17:19:21 +0900

    추가적으로 구현한 부분 한계 : 기존에 만든 코드는 mnist로만 학습한 모델을 사용해 mnist 규격에서 벗어나는 형태의 숫자에는 낮은 인식률을 보였다. 따라서 추가적인 데이터를 수집하고자 몇 가지 기능을 구현했다. 1. 임의의 숫자를 지정해주는 위젯 2. 위젯을 보고 쓴 숫자의 정답 여부에 따라 right, no 버튼을 누름 3. 아무 것도 쓰지 않았는데 누른다면 1이 변하지 않은 채 숫자를 써달라는 메시지를 띄움 4. 저장의 편리성을 위해 튜플로 받음(제시된 임의의 수 , 예측 값, 그림 as np)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import numpy as np
+import random
 import torch
 import net
 
@@ -14,13 +15,19 @@ class MyApp(QMainWindow):
         self.image = QImage(QSize(400, 400), QImage.Format_RGB32)
         self.image.fill(Qt.white)
         self.drawing = False
-        self.brush_size = 30
+        self.brush_size = 20
         self.brush_color = Qt.black
         self.last_point = QPoint()
         self.loaded_model = None
+        self.n = random.randrange(0, 10)
+        self.txt = ()
+        self.fname ='/Users/ijimin/Desktop/지민/pycharm/pyqt test/MNIST_CNNmodel_99%_state.pth'
+        self.arr = np.zeros((28,28))
+        self.predicted = 0
         self.initUI()
 
     def initUI(self):
+        #메뉴바_ load, save, clear
         menubar = self.menuBar()
         menubar.setNativeMenuBar(False)
         filemenu = menubar.addMenu('File')
@@ -43,9 +50,43 @@ class MyApp(QMainWindow):
 
         self.statusbar = self.statusBar()
 
-        self.setWindowTitle('MNIST Classifier')
+        #yes, no button 만들기
+        self.formatbar = QToolBar(self)
+        self.addToolBar(Qt.BottomToolBarArea, self.formatbar)
+
+        btn_1 = QToolButton(self)
+        btn_2 = QToolButton(self)
+
+        btn_1.setText('Right')
+        btn_1.setCheckable(False)
+        btn_2.setText('Wrong')
+        btn_2.setCheckable(False)
+
+        btn_1.clicked.connect(self.clear)
+        btn_1.clicked.connect(self.empty_or_not)
+
+        btn_2.clicked.connect(self.save_wrong)
+        btn_2.clicked.connect(self.clear)
+
+
+        self.formatbar.addWidget(btn_1)
+        self.formatbar.addWidget(btn_2)
+
+        #숫자 지정해주기
+        self.num_label = QLabel(f'{self.n} 써주세요', self)
+        self.num_label.move(155,0)
+
+        if self.fname:
+            self.loaded_model=torch.load(self.fname)
+            self.statusbar.showMessage('Model loaded')
+
+        self.setWindowTitle('MNIST_classifier')
         self.setGeometry(300, 300, 400, 400)
         self.show()
+
+    def on_click(self):
+        self.n = random.randrange(0,10)
+        self.num_label.setText(f'{self.n} 써주세요')
 
     def paintEvent(self, e):
         canvas = QPainter(self)
@@ -67,15 +108,13 @@ class MyApp(QMainWindow):
     def mouseReleaseEvent(self, e):
         if e.button() == Qt.LeftButton:
             self.drawing = False
-
-            arr = np.zeros((28, 28))
+            self.arr = np.zeros((28, 28))
             for i in range(28):
                 for j in range(28):
-                    arr[j, i] = 1 - self.image.scaled(28, 28).pixelColor(i, j).getRgb()[0] / 255.0
-            arr = arr.reshape(-1, 28, 28)
-            tensor = torch.from_numpy(arr).float()
+                    self.arr[j, i] = 1 - self.image.scaled(28, 28).pixelColor(i, j).getRgb()[0] / 255.0
+            self.arr = self.arr.reshape(-1, 28, 28)
+            tensor = torch.from_numpy(self.arr).float()
             x = tensor.unsqueeze(dim=0)
-
 
             if self.loaded_model:
                 net.model.load_state_dict(self.loaded_model)
@@ -83,25 +122,40 @@ class MyApp(QMainWindow):
 
                 with torch.no_grad():
                     pred = net.model(x)
-                    print(type(pred))
                     ans = torch.argmax(pred).item()
-                    predicted = net.classes[ans]
-                    self.statusbar.showMessage("추정 값은" + predicted + "입니다.")
+                    self.predicted = net.classes[ans]
+                    self.statusbar.showMessage("추정 값은" + self.predicted + "입니다.")
 
     def load_model(self):
         fname, _ = QFileDialog.getOpenFileName(self, 'Load Model', '')
-
         if fname:
             self.loaded_model = torch.load(fname)
             self.statusbar.showMessage('Model loaded.')
 
-
     def save(self):
-        fpath, _ = QFileDialog.getSaveFileName(self, 'Save Image', '', "PNG(*.png);;JPEG(*.jpg *.jpeg);;All Files(*.*) ")
-
+        fpath, _ = QFileDialog.getSaveFileName(self, 'Save Image', '',
+                                               "PNG(*.png);;JPEG(*.jpg *.jpeg);;All Files(*.*) ")
         if fpath:
             self.image.scaled(28, 28).save(fpath)
+    def save_wrong(self):
+        if np.any(self.arr.flatten()>0.) == False:
+            print('write something')
+            self.statusbar.showMessage('숫자를 쓴 후 눌러주세요.')
+            # method empty_or_not 안에 있는 거랑 진짜 똑같은데 왜 안될까
 
+        else:
+            test_num_string = self.n
+            input_arr = self.arr
+            output_num = int(self.predicted)
+            s = (test_num_string,output_num,input_arr)
+            self.txt += s
+            self.on_click()
+    def empty_or_not(self):
+        if np.any(self.arr.flatten()>0.) == False:
+            print('write something')
+            self.statusbar.showMessage('숫자를 쓴 후 눌러주세요.')
+        else:
+            self.on_click()
     def clear(self):
         self.image.fill(Qt.white)
         self.update()
@@ -111,4 +165,5 @@ class MyApp(QMainWindow):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyApp()
+    ex.show()
     sys.exit(app.exec_())
